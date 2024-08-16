8ce90c01fbed1d983d7f2fd7bb0660e899e21d19
Diego Olmi <diegolmi@hotmail.com>
2024-08-16 16:00:49 -0300

    redireccion

--- src/components/InviteForm.jsx
+++ src/components/InviteForm.jsx
@@ -5,16 +5,28 @@ const InviteForm = () => {
   const [name, setName] = useState('');
   const [phone, setPhone] = useState('');
 
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   try {
+  //     await axios.post('http://localhost:8081/api/invite', { name, phone });
+  //     alert('Invitation sent');
+  //   } catch (error) {
+  //     console.error('There was an error sending the invitation:', error);
+  //     alert('Failed to send invitation. Please try again.');
+  //   }
+  // };
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post('http://localhost:8081/api/invite', { name, phone });
+      const response = await axios.post('http://localhost:8081/api/invite', { name, phone });
+      console.log(response.data);
       alert('Invitation sent');
     } catch (error) {
       console.error('There was an error sending the invitation:', error);
       alert('Failed to send invitation. Please try again.');
     }
   };
+  
 
   return (
     <>
