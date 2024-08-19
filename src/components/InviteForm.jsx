8bf2b1acdcdea54e4559faf8de16cdd1b521b734
Diego Olmi <diego.olmi@gmail.com>
2024-08-19 10:23:49 -0300

    comment

--- src/components/InviteForm.jsx
+++ src/components/InviteForm.jsx
@@ -24,7 +24,7 @@ const InviteForm = () => {
     <>
     <Header />
     <Hero />
-    <h1>Invitación Casamiento</h1>
+    {/* <h1>Invitación Casamiento</h1> */}
     {/* <form onSubmit={handleSubmit}>
       <input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} required />
       <input type="tel" placeholder="Phone" value={phone} onChange={(e) => setPhone(e.target.value)} required />
